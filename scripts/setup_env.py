d2b407576210c9c9bc91604017bdc74f49b397b3
Daniel Büchele <daniel@buechele.cc>
2020-06-15 23:59:46 +0100

    env script

--- scripts/setup_env.py
+++ scripts/setup_env.py
@@ -1,11 +1,15 @@
 Import("env")
+import os.path
 
 def parse_line(line):
-  name, val = line.split('=', 1)
-  val = val.strip().replace('"', '\\"')
-  return (name.strip(), val.strip())
+    name, val = line.split("=", 1)
+    val = val.strip().replace('"', '\\"')
+    return (name.strip(), val.strip())
 
-file = open('./.env', 'r')
-lines = [line.rstrip('\n') for line in file]
-variables = map(parse_line, lines)
-env.Append(CPPDEFINES=variables)
+filename = "./.envd"
+if os.path.exists(filename):
+    file = open(filename, "r")
+    lines = [line.rstrip("\n") for line in file]
+    variables = map(parse_line, lines)
+    env.Append(CPPDEFINES=variables)
+    file.close()
