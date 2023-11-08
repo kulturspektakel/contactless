47084c9241c1390e16244745fb1146fd493c90cf
Daniel Büchele <daniel@buechele.cc>
2023-11-08 22:00:06 +0000

    keypad

--- main/keypad.c
+++ main/keypad.c
@@ -1,30 +1,88 @@
 #include "keypad.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
+#include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
 #include "freertos/task.h"
 
-#define INPUT_PIN 27
 static const char* TAG = "display";
+const char KEYPAD[] = {
+    // clang-format off
+    '1', '2', '3', 'A',
+    '4', '5', '6', 'B',
+    '7', '8', '9', 'C',
+    '*', '0', '#', 'D'
+    // clang-format on
+};
+static const int KEYPAD_PINS[8] = {2, 4, 16, 17, 5, 18, 19, 21};
+
+int64_t time_old_isr = 0;
+QueueHandle_t keypad_queue;
+
+void turnon_rows() {
+  for (int i = 4; i < 8; i++) {  // Columns
+    gpio_set_pull_mode(KEYPAD_PINS[i], GPIO_PULLDOWN_ONLY);
+  }
+  for (int i = 0; i < 4; i++) {  // Rows
+    gpio_set_pull_mode(KEYPAD_PINS[i], GPIO_PULLUP_ONLY);
+    gpio_intr_enable(KEYPAD_PINS[i]);
+  }
+}
+
+void turnon_cols() {
+  for (int i = 0; i < 4; i++) {  // Rows
+    gpio_intr_disable(KEYPAD_PINS[i]);
+    gpio_set_pull_mode(KEYPAD_PINS[i], GPIO_PULLDOWN_ONLY);
+  }
+  for (int i = 4; i < 8; i++) {  // Columns
+    gpio_set_pull_mode(KEYPAD_PINS[i], GPIO_PULLUP_ONLY);
+  }
+}
 
 static void IRAM_ATTR gpio_interrupt_handler(void* args) {
-  // notify the task that the interrupt occurred
-  xTaskNotifyFromISR(xTaskGetHandle("keypad"), 1, eSetBits, NULL);
+  int r = (int)(args);
+  int64_t time_now_isr = esp_timer_get_time();
+
+  // if (time_now_isr - time_old_isr >= 100000) {
+  turnon_cols();
+  for (int c = 4; c < 8; c++) {
+    if (!gpio_get_level(KEYPAD_PINS[c])) {
+      xQueueSendFromISR(keypad_queue, &KEYPAD[r * 4 + c - 4], NULL);
+      break;
+    }
+  }
+  turnon_rows();
+  // }
+  // time_old_isr = time_now_isr;
 }
 
 void keypad(void* params) {
-  esp_rom_gpio_pad_select_gpio(INPUT_PIN);
-  gpio_set_direction(INPUT_PIN, GPIO_MODE_INPUT);
-  gpio_pulldown_en(INPUT_PIN);
-  gpio_pullup_dis(INPUT_PIN);
-  gpio_set_intr_type(INPUT_PIN, GPIO_INTR_POSEDGE);
+  keypad_queue = xQueueCreate(5, sizeof(char));
+  if (keypad_queue == NULL) {
+    ESP_LOGI(TAG, "Failed to create keypad queue");
+    // TODO fatal error
+    return;
+  }
 
-  gpio_install_isr_service(0);
-  gpio_isr_handler_add(INPUT_PIN, gpio_interrupt_handler, (void*)INPUT_PIN);
+  ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_install_isr_service(ESP_INTR_FLAG_EDGE));
+  for (int r = 0; r < 4; r++) {  // Rows
+    gpio_intr_disable(KEYPAD_PINS[r]);
+    gpio_set_direction(KEYPAD_PINS[r], GPIO_MODE_INPUT);
+    gpio_set_intr_type(KEYPAD_PINS[r], GPIO_INTR_NEGEDGE);
+    ESP_ERROR_CHECK_WITHOUT_ABORT(
+        gpio_isr_handler_add(KEYPAD_PINS[r], (void*)gpio_interrupt_handler, (void*)r)
+    );
+  }
+  for (int c = 4; c < 8; c++) {  // Columns
+    gpio_set_direction(KEYPAD_PINS[c], GPIO_MODE_INPUT);
+  }
 
+  turnon_rows();
+
+  char key;
   while (true) {
-    // wait for the interrupt to occur
-    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-    ESP_LOGI(TAG, "INTERRRPUT ");
+    xQueueReceive(keypad_queue, &key, portMAX_DELAY);
+    ESP_LOGI(TAG, "Keypressed %c", key);
   }
 }
--- main/time_sync.c
+++ main/time_sync.c
@@ -12,24 +12,22 @@ void time_sync(void* params) {
   setenv("TZ", "CET-1CEST,M3.5.0,M10.5.0/3", 1);
   tzset();
 
+  // TODO read from RTC
+
   // wait for wifi to connect
-  xEventGroupWaitBits(event_group, WIFI_CONNECTED_BIT, false, true, portMAX_DELAY);
+  xEventGroupWaitBits(event_group, WIFI_CONNECTED, false, true, portMAX_DELAY);
 
   esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG("pool.ntp.org");
   esp_netif_sntp_init(&config);
 
   // wait for time to be set
-  time_t now = 0;
-  struct tm timeinfo = {0};
-  int retry = 0;
-  const int retry_count = 15;
-  while (esp_netif_sntp_sync_wait(2000 / portTICK_PERIOD_MS) == ESP_ERR_TIMEOUT &&
-         ++retry < retry_count) {
-    ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+  int retries = 15;
+  while (esp_netif_sntp_sync_wait(2000 / portTICK_PERIOD_MS) == ESP_ERR_TIMEOUT && retries-- > 0) {
+    ESP_LOGI(TAG, "Waiting for system time to be set... (%d)", retries);
   }
-  time(&now);
-  localtime_r(&now, &timeinfo);
-
-  ESP_ERROR_CHECK(example_disconnect());
   esp_netif_sntp_deinit();
+
+  // TODO set RTC
+
+  vTaskDelete(NULL);
 }
