672cdd40213795c408ce4451daad7bed5d73f52f
Daniel Büchele <daniel@buechele.cc>
2024-01-09 23:48:02 +0000

    pwr

--- main/power_management.c
+++ main/power_management.c
@@ -1,34 +1,48 @@
 #include "power_management.h"
 #include "driver/gpio.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_event.h"
 #include "esp_log.h"
+#include "event_group.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 #define TAG "power_management"
-#define BATTERY_VOLTAGE_PIN GPIO_NUM_1
-#define USB_VOLTAGE_PIN GPIO_NUM_2
+#define BATTERY_VOLTAGE ADC1_CHANNEL_0
+#define USB_VOLTAGE ADC1_CHANNEL_1
 #define LED_BLUE_PIN GPIO_NUM_40
 #define LED_GREEN_PIN GPIO_NUM_41
 #define LED_RED_PIN GPIO_NUM_42
 
-void power_management(void* params) {
-  gpio_config_t io_conf;
-  io_conf.intr_type = GPIO_INTR_DISABLE;
-  io_conf.mode = GPIO_MODE_INPUT;
-  io_conf.pin_bit_mask = (1ULL << BATTERY_VOLTAGE_PIN) | (1ULL << USB_VOLTAGE_PIN);
-  io_conf.pull_down_en = 0;
-  io_conf.pull_up_en = 1;
-  gpio_config(&io_conf);
+static int adc_raw[2];
+int battery_voltage = 0;
+int usb_voltage = 0;
 
-  io_conf.pin_bit_mask = (1ULL << LED_BLUE_PIN) | (1ULL << LED_GREEN_PIN) | (1ULL << LED_RED_PIN);
-  io_conf.mode = GPIO_MODE_OUTPUT;
-  gpio_config(&io_conf);
+void power_management(void* params) {
+  adc_oneshot_unit_handle_t adc1_handle;
+  adc_oneshot_unit_init_cfg_t init_config1 = {
+      .unit_id = ADC_UNIT_1,
+      .ulp_mode = ADC_ULP_MODE_DISABLE,
+  };
+  ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+
+  adc_oneshot_chan_cfg_t config = {
+      .bitwidth = ADC_BITWIDTH_DEFAULT,
+      .atten = ADC_ATTEN_DB_11,
+  };
+  ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_0, &config));
+  ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_1, &config));
 
   while (true) {
-    int battery_voltage = gpio_get_level(BATTERY_VOLTAGE_PIN);
-    int usb_voltage = gpio_get_level(USB_VOLTAGE_PIN);
+    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_0, &adc_raw[0]));
+    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, ADC_CHANNEL_1, &adc_raw[1]));
+    ESP_LOGI(TAG, "ADC channel 0: %d, channel 1: %d", adc_raw[0], adc_raw[1]);
+    usb_voltage = adc_raw[0];
+    battery_voltage = adc_raw[1];
 
-    ESP_LOGI(TAG, "battery_voltage: %d, usb_voltage: %d", battery_voltage, usb_voltage);
+    xEventGroupSetBits(event_group, DISPLAY_NEEDS_UPDATE);
     vTaskDelay(pdMS_TO_TICKS(1000));
   }
+
+  ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
 }
--- main/power_management.h
+++ main/power_management.h
@@ -1,3 +1,5 @@
 #pragma once
 
+extern int battery_voltage;
+extern int usb_voltage;
 void power_management(void* params);
